cabe51fd966cc5921cf58887d5c2b33a7420950a
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-11-24 21:16:03 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Backdrop from "./components/Backdrop/Backdrop";
+
+function App() {
+  return (
+    <div className="h-full bg-slate-900 flex flex-col items-center justify-center">
+    </div>
+  );
+}
+
+export default App;
